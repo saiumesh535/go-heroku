9dfe829acb59d99bea396a3493b2568b69b20cad
saiumesh-letznav <35692863+saiumesh-letznav@users.noreply.github.com>
2019-06-23 12:44:22 +0530

    first commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module go-heroku
+
+go 1.12
+
+require (
+	github.com/gin-gonic/gin v1.4.0
+	github.com/labstack/gommon v0.2.9
+)
---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/labstack/gommon/log"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	gin.SetMode(gin.ReleaseMode)
+	r := gin.New()
+	r.Use(gin.Recovery())
+	r.GET("/", func(context *gin.Context) {
+		context.JSON(http.StatusOK, gin.H{
+			"hey!": "Message from Heroku",
+		})
+	})
+	err := r.Run(":" + port)
+	if err != nil {
+		log.Fatal("error in starting server", err)
+	}
+}

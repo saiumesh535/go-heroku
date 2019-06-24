cb11e3b6f9364535627095660ba7797b74555e0b
saiumesh-letznav <35692863+saiumesh-letznav@users.noreply.github.com>
2019-06-24 11:07:55 +0530

    moved to echo

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.12
 
 require (
 	github.com/gin-gonic/gin v1.4.0
+	github.com/labstack/echo/v4 v4.1.6
 	github.com/labstack/gommon v0.2.9
 )
--- main.go
+++ main.go
@@ -4,22 +4,19 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/gin-gonic/gin"
-	"github.com/labstack/gommon/log"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
 	port := os.Getenv("PORT")
-	gin.SetMode(gin.ReleaseMode)
-	r := gin.New()
-	r.Use(gin.Recovery())
-	r.GET("/", func(context *gin.Context) {
-		context.JSON(http.StatusOK, gin.H{
-			"hey!": "Message from Heroku, with restart now 🔥🔥",
-		})
-	})
-	err := r.Run(":" + port)
-	if err != nil {
-		log.Fatal("error in starting server", err)
-	}
+	e := echo.New()
+	e.Use(middleware.Recover())
+	e.GET("/", hello)
+	e.Logger.Fatal(e.Start(":" + port))
+}
+
+// Handler
+func hello(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, World!")
 }
